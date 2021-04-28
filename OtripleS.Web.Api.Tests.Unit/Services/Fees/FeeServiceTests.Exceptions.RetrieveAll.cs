bbf74a631e6f904535225ab2301229986e247734
Josh McCall <joshmccall221@gmail.com>
2021-04-28 01:23:08 -0500

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllAndLogIt -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/Fees/FeeServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,51 @@
+﻿//---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+//----------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Moq;
+using OtripleS.Web.Api.Models.Fees.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.Fees
+{
+    public partial class FeeServiceTests
+    {
+        [Fact]
+        public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllAndLogIt()
+        {
+            // given
+            SqlException sqlException = GetSqlException();
+            var expectedFeeDependencyException = new FeeDependencyException(sqlException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllFees())
+                    .Throws(sqlException);
+
+            // when 
+            Action retrieveAllFeesAction = () => this.feeService.RetrieveAllFees();
+
+            // then 
+            Assert.Throws<FeeDependencyException>(retrieveAllFeesAction);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllFees(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(
+                    SameExceptionAs(expectedFeeDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
